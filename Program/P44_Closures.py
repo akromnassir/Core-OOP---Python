3396f34f97120917b7911a6f6c8e7aac63b6cdc3
CodeWithDjan <85175356+akromnassir@users.noreply.github.com>
2025-08-04 09:31:37 +0500

    Update P44_Closures.py

--- Program/P44_Closures.py
+++ Program/P44_Closures.py
@@ -2,8 +2,8 @@
 # Wikipedia:
 # A closure is a record storing a function[a] together with an environment:
 # a mapping associating each free variable of the function (variables that are used locally, but
-# defined in an enclosing scope) with the value or reference to which the name was bound when
-# the closure was created.A closure—unlike a plain function—allows the function to access those
+# defined in an enclosing scope) With the value or reference to which the name was bound when
+# The closure was created.A closure—unlike a plain function—allows the function to access those
 # captured variables through the closure's copies of their values or references, even when the function
 # is invoked outside their scope.
 
