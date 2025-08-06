77582f0f129c05b9a0209990d0fe895b2145ee35
CodeWithDjan <85175356+akromnassir@users.noreply.github.com>
2025-08-06 10:00:11 +0500

    Update P47_MoreOnDecorators.py

--- Program/P47_MoreOnDecorators.py
+++ Program/P47_MoreOnDecorators.py
@@ -1,6 +1,6 @@
 
-# In this example, we will be seeing some more concepts of decorators such as
-# property decorator, getters and setters methods.
+# In this example, we will be seeing some more concepts of decorators, such as
+# property decorator, getters, and setter methods.
 
 class BankAccount(object):
     def __init__(self, firstName, lastName):
@@ -20,8 +20,8 @@ class BankAccount(object):
 if __name__ == '__main__':
     acc = BankAccount('Omkar', 'Pathak')
     print(acc.fullName)              # Notice that we can access the method for our class BankAccount without
-                                     # parenthesis! This is beacuse of property decorator
+                                     # parentheses! This is because of the property decorator
 
-    # acc.fullName = 'Omkar Pathak'    #This throws an error! Hence setter decorator should be used.
+    # acc.fullName = 'Omkar Pathak'    #This throws an error! Hence, setter decorator should be used.
     acc.fullName = 'Jagdish Pathak'
     print(acc.fullName)
