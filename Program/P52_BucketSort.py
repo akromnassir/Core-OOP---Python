a96e241c131a862bfed8feaa5799bb91465049c3
CodeWithDjan <85175356+akromnassir@users.noreply.github.com>
2025-08-14 15:43:56 +0500

    Update P52_BucketSort.py

--- Program/P52_BucketSort.py
+++ Program/P52_BucketSort.py
@@ -1,5 +1,5 @@
 
-# This program will illustrate how to implement bucket sort algorithm
+# This program will illustrate how to implement the bucket sort algorithm
 
 # Wikipedia says: Bucket sort, or bin sort, is a sorting algorithm that works by distributing the
 # elements of an array into a number of buckets. Each bucket is then sorted individually, either using 
