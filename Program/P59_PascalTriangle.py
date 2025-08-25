c9d15c491a1489eb8f5143fe7f66807f70a81672
CodeWithDjan <85175356+akromnassir@users.noreply.github.com>
2025-08-25 09:29:16 +0500

    Update P59_PascalTriangle.py

--- Program/P59_PascalTriangle.py
+++ Program/P59_PascalTriangle.py
@@ -1,5 +1,4 @@
 
-
 # PASCAL TRIANGLE: To build the triangle, start with "1" at the top, then continue placing numbers
 # below it in a triangular pattern. Each number is the number directly above it.
 
