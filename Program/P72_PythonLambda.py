55f8d50e98faf8a320c61345166a4efb922b72f8
CodeWithDjan <85175356+akromnassir@users.noreply.github.com>
2025-09-09 20:47:01 +0500

    Update P72_PythonLambda.py

--- Program/P72_PythonLambda.py
+++ Program/P72_PythonLambda.py
@@ -1,11 +1,11 @@
 
 # In this program, we will learn what Python lambda is.
 # The lambda operator or lambda function is a way to create small anonymous functions, i.e., functions without
-# a name. These functions are throw-away functions, i.e. they are just needed where they have been created.
+# a name. These functions are throw-away functions, i.e., they are just needed where they have been created.
 # Lambda functions are mainly used in combination with the functions filter(), map() and reduce(). The lambda
 # feature was added to Python due to the demand from Lisp programmers.
 
-# The argument list consists of a comma separated list of arguments and the expression is an arithmetic
+# The argument list consists of a comma-separated list of arguments and the expression is an arithmetic
 # expression using these arguments. You can assign the function to a variable to give it a name.
 # The following example of a lambda function returns the sum of its two arguments:
 
