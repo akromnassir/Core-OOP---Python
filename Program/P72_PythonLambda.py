28454ba4a3c7a72b62f765545ee63ce8aa55ed69
CodeWithDjan <85175356+akromnassir@users.noreply.github.com>
2025-09-09 20:47:27 +0500

    Update P72_PythonLambda.py

--- Program/P72_PythonLambda.py
+++ Program/P72_PythonLambda.py
@@ -5,7 +5,7 @@
 # Lambda functions are mainly used in combination with the functions filter(), map() and reduce(). The lambda
 # feature was added to Python due to the demand from Lisp programmers.
 
-# The argument list consists of a comma-separated list of arguments and the expression is an arithmetic
+# The argument list consists of a comma-separated list of arguments, and the expression is an arithmetic
 # expression using these arguments. You can assign the function to a variable to give it a name.
 # The following example of a lambda function returns the sum of its two arguments:
 
