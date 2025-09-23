5a5e7bfecfe8e0caf2e1f91c890dc0cd9fd8c00f
CodeWithDjan <85175356+akromnassir@users.noreply.github.com>
2025-09-23 16:36:01 +0500

    Update pyxhook.py

--- Program/pyxhook.py
+++ Program/pyxhook.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
 # pyxhook -- an extension to emulate some of the PyHook library on Linux.
-#
 
-#
 #    This program is free software; you can redistribute it and/or modify it
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
